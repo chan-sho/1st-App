1d0711f905f9901621eb958c50aad5a8077fcb85
高野翔 <ShoTakano@takanoshou-no-macbook-pro.local>
2018-07-29 00:33:02 +0900

    2018/7/29  1st

--- 1st app/ViewController.swift
+++ 1st app/ViewController.swift
@@ -11,14 +11,22 @@ import MapKit
 
 class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var buttonForsearchPlace: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         inputText.delegate = self
+        
         label.text = "↑ STEP1: 投稿する場所を検索"
         label.textColor = UIColor.blue // 青
         label.backgroundColor = UIColor.white // 白
+        
+        buttonForsearchPlace.setTitle("住所を検索して投稿", for: UIControlState.normal)
+        buttonForsearchPlace.setTitleColor(UIColor.red, for: UIControlState.normal)
+        buttonForsearchPlace.layer.borderColor = UIColor.red.cgColor
+        buttonForsearchPlace.layer.borderWidth = 1.0
+        buttonForsearchPlace.layer.cornerRadius = 10.0 //丸みを数値で変更できる
     }
     
     override func didReceiveMemoryWarning() {
